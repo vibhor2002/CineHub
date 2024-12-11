ecb52d76f1b3d7c265ce5b9c5b6b4bc74e7673c1
Vibhor Grover <grovervibhor21@gmail.com>
2024-12-11 15:59:50 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  const [query, setQuery] = useState(""); // State lifted to App
+  const [query, setQuery] = useState("");
 
   return (
     <>
